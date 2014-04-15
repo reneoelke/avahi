07091abd5960919d06c0c29ee6761d5372e9fcfb
mhaeussler-magari <marcus.haeussler@magari-internet.de>
2014-04-15 14:30:28 +0200

    added name attribute for berkshelf 3

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ license           "Apache 2.0"
 description       "Installs and configures avahi daemon."
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.1.4"
+name              "avahi"
 
 recipe            "default", "Common installation and configuration."
 recipe            "aliases", "Common installation and configuration for avahi aliases."
